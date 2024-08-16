8144db3dfecbfd5182159a47502a335f56b81299
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-08-16 17:59:09 +0530

    query stock details

--- src/app/[symbol].tsx
+++ src/app/[symbol].tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { ActivityIndicator, StyleSheet } from 'react-native'
 import { Text, View } from '../components/Themed'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
@@ -6,14 +6,35 @@ import top5 from "@/assets/data/top5.json"
 import StockListItem from '../components/StockListItem'
 import Graph from '../components/Graph'
 
+import { useQuery, gql } from '@apollo/client';
+
+const query = gql`
+  query MyQuery($symbol: String = "") {
+  quote(symbol: $symbol) {
+    name
+    symbol
+    close
+    percent_change
+  }
+}
+`;
+
 const StockDetails = () => {
   const { symbol } = useLocalSearchParams();
 
-  const stock = top5[symbol];
+  const { data, loading, error } = useQuery(query, {variables: {symbol: symbol}});
 
-  if (!stock) {
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
+  if (error) {
     return <Text>Stock with symbol {symbol} could not be found</Text>
   }
+
+  const stock = data.quote;
+
+  console.log(stock)
 
   return (
     <View>
